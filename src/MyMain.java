d40cd8517976109a951ae1590c4b332ee4ca5f97
CAHL <cahl@kea.dk>
2022-05-03 11:05:36 +0200

    ny sout 33

--- src/MyMain.java
+++ src/MyMain.java
@@ -5,5 +5,6 @@ public class MyMain {
         Person peter = new Person("Peter", "Hansen", 22);
         System.out.println(peter);
         System.out.println("Ny version");
+        System.out.println("Ny version2222");
     }
 }
