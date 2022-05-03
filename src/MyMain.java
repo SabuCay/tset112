790d5ee6cbcf4785c5be58463003b9f0f426e9e1
CAHL <cahl@kea.dk>
2022-05-03 10:56:27 +0200

    Initial commit

---
+++ src/MyMain.java
@@ -0,0 +1,8 @@
+public class MyMain {
+    public static void main(String[] args) {
+        System.out.println("Hej James");
+
+        Person peter = new Person("Peter", "Hansen", 22);
+        System.out.println(peter);
+    }
+}
